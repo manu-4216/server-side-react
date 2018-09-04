9d5961a6657220d49f89a3abf1de586d6ab3d428
manu-4216 <manu4216@gmail.com>
2018-09-04 18:21:57 +0200

    Use webpack-merge to share the common webpack config

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,9 @@
 const path = require('path')
 
-module.exports = {
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.base.js')
+
+const config = {
   // Inform webpack that we're building a bundle for nodeJS,
   // rather then for the browser
   target: 'node',
@@ -12,29 +15,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  },
-
-  // Tell webpack to run babel on every file it runs through
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react',
-            [
-              'env',
-              {
-                targets: {
-                  browsers: ['last 2 versions']
-                }
-              }
-            ]
-          ]
-        }
-      }
-    ]
   }
 }
+
+module.exports = merge(baseConfig, config)
