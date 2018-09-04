afa6f8c051b477694b45978b2c2a932aeccda4de
manu-4216 <manu4216@gmail.com>
2018-09-04 18:43:25 +0200

    Exclude node_module lib code from server bundle

--- webpack.server.js
+++ webpack.server.js
@@ -1,7 +1,7 @@
 const path = require('path')
-
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.base.js')
+const webpackNodeExternals = require('webpack-node-externals')
 
 const config = {
   // Inform webpack that we're building a bundle for nodeJS,
@@ -15,7 +15,10 @@ const config = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  }
+  },
+
+  // Don't include inside the bundle the code of the libraries from node_modules
+  externals: [webpackNodeExternals()]
 }
 
 module.exports = merge(baseConfig, config)
