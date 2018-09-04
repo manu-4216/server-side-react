79ffb020c25e1d305bfee936d63e01a305ac30a3
manu-4216 <manu4216@gmail.com>
2018-09-04 23:03:46 +0200

    Add redux for fetching the users client side

---
+++ src/client/components/UsersList.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchUsers } from '../actions'
+
+class UsersList extends Component {
+  componentDidMount() {
+    this.props.fetchUsers()
+  }
+
+  renderUsers() {
+    return this.props.users.map(user => <li key={user.id}>{user.name}</li>)
+  }
+
+  render() {
+    return (
+      <div>
+        Here are the users:
+        <ul>{this.renderUsers()}</ul>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  users: state.users
+})
+
+export default connect(
+  mapStateToProps,
+  { fetchUsers }
+)(UsersList)
