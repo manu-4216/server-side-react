f5fd3e3251f88e2802c37f55db178485c07cf866
manu-4216 <manu4216@gmail.com>
2018-09-04 12:07:41 +0200

    Add public client.js bundle to hydrate the server html
    Steps:
     - Create a 2nd webpack config file for client
     - Server side, inject a ref to the new client bundle
     - Inside the client.js, hydrate the static html

--- build/bundle.js
+++ build/bundle.js
@@ -7019,10 +7019,14 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var app = (0, _express2.default)();
 
+app.use(_express2.default.static('public'));
+
 app.use('/', function (req, res) {
   var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
 
-  res.send(content);
+  var html = '\n    <html>\n      <head></head>\n      <body>\n        <div id="root">' + content + '</div>\n        <script src="bundle.js"></script>\n      </body>\n    </html>\n  ';
+
+  res.send(html);
 });
 
 app.listen(3000, function () {
@@ -22140,7 +22144,18 @@ var Home = function Home() {
   return _react2.default.createElement(
     'div',
     null,
-    'I\'m the cool home component'
+    _react2.default.createElement(
+      'div',
+      null,
+      'I\'m the cool home component'
+    ),
+    _react2.default.createElement(
+      'button',
+      { onClick: function onClick() {
+          return console.log('Hello');
+        } },
+      'Press me!'
+    )
   );
 };
 
--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const Home = () => {
-  return <div>I'm the cool home component</div>
+  return (
+    <div>
+      <div>I'm the cool home component</div>
+      <button onClick={() => console.log('Hello')}>Press me!</button>
+    </div>
+  )
 }
 
 export default Home
