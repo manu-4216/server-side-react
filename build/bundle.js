000ba6a14b0706f1b9a2a61e1e06a31a65f16cd8
manu-4216 <manu4216@gmail.com>
2018-09-05 00:36:28 +0200

    Use react-router-config to know which Route to render from an URL

--- build/bundle.js
+++ build/bundle.js
@@ -178,6 +178,8 @@ var _reactRouterDom = __webpack_require__(7);
 
 var _reactRedux = __webpack_require__(12);
 
+var _reactRouterConfig = __webpack_require__(19);
+
 var _Routes = __webpack_require__(8);
 
 var _Routes2 = _interopRequireDefault(_Routes);
@@ -191,7 +193,11 @@ exports.default = function (req, store) {
     _react2.default.createElement(
       _reactRouterDom.StaticRouter,
       { location: req.path, context: {} },
-      _react2.default.createElement(_Routes2.default, null)
+      _react2.default.createElement(
+        'div',
+        null,
+        (0, _reactRouterConfig.renderRoutes)(_Routes2.default)
+      )
     )
   ));
 
@@ -219,8 +225,6 @@ var _react = __webpack_require__(0);
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactRouterDom = __webpack_require__(7);
-
 var _Home = __webpack_require__(4);
 
 var _Home2 = _interopRequireDefault(_Home);
@@ -231,14 +235,14 @@ var _UsersList2 = _interopRequireDefault(_UsersList);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = function () {
-  return _react2.default.createElement(
-    'div',
-    null,
-    _react2.default.createElement(_reactRouterDom.Route, { exact: true, path: '/', component: _Home2.default }),
-    _react2.default.createElement(_reactRouterDom.Route, { path: '/users', component: _UsersList2.default })
-  );
-};
+exports.default = [{
+  path: '/',
+  component: _Home2.default,
+  exact: true
+}, {
+  path: '/users',
+  component: _UsersList2.default
+}];
 
 /***/ }),
 /* 9 */
@@ -482,6 +486,12 @@ exports.default = (0, _reactRedux.connect)(mapStateToProps, { fetchUsers: _actio
 /***/ (function(module, exports) {
 
 module.exports = require("babel-polyfill");
+
+/***/ }),
+/* 19 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-router-config");
 
 /***/ })
 /******/ ]);
