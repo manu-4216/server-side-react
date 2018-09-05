891be79de0bff774e4b11f821ae175252b5f3a62
manu-4216 <manu4216@gmail.com>
2018-09-05 16:29:52 +0200

    Add some styling with materializecss lib

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <div>I'm the cool home component</div>
-      <button onClick={() => console.log('Hello')}>Press me!</button>
+    <div className="center-align" style={{ marginTop: '200px' }}>
+      <h3>Welcome</h3>
+      <p>Check out these awesome features</p>
     </div>
   )
 }
