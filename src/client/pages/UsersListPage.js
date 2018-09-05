5d72f585675d146e7c5cecdc98b01321e014aecf
manu-4216 <manu4216@gmail.com>
2018-09-05 19:53:03 +0200

    Add react-helmet to declare title and og-tags dynamically

--- src/client/pages/UsersListPage.js
+++ src/client/pages/UsersListPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchUsers } from '../actions'
+import { Helmet } from 'react-helmet'
 
 class UsersList extends Component {
   componentDidMount() {
@@ -11,9 +12,19 @@ class UsersList extends Component {
     return this.props.users.map(user => <li key={user.id}>{user.name}</li>)
   }
 
+  head() {
+    return (
+      <Helmet>
+        <title>{`${this.props.users.length} Users Loaded`}</title>
+        <meta property="og:title" content="Users List" />
+      </Helmet>
+    )
+  }
+
   render() {
     return (
       <div>
+        {this.head()}
         Here are the users:
         <ul>{this.renderUsers()}</ul>
       </div>
