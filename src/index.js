b1fbc435d28832e14e7abfe044c3eb49b4c03394
manu-4216 <manu4216@gmail.com>
2018-09-05 14:13:49 +0200

    Use the render server as a proxy to handle the auth to the api server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import express from 'express'
+import proxy from 'express-http-proxy'
 import { matchRoutes } from 'react-router-config'
 import Routes from './client/Routes'
 import 'babel-polyfill'
 import renderer from './helpers/renderer'
 import createStore from './helpers/createStore'
+
 const app = express()
+
+app.use(
+  '/api',
+  proxy('http://react-ssr-api.herokuapp.com', {
+    proxyReqOptDecorator(opts) {
+      opts.header['x-forwarded-host'] = 'localhost:3000'
+      return opts
+    }
+  })
+)
 
 app.use(express.static('public'))
 
