e5da8e74155b8f0c512b5d7aa35c465c44807509
manu-4216 <manu4216@gmail.com>
2018-09-05 16:57:45 +0200

    Use staticContext to set 404 status on not found page

--- src/client/pages/NotFoundPage.js
+++ src/client/pages/NotFoundPage.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
-const NotFoundPage = () => <h2>Oooops, route not found.</h2>
+const NotFoundPage = ({ staticContext = {} }) => {
+  staticContext.notFound = true
+
+  return <h2>Oooops, route not found.</h2>
+}
 
 export default {
   component: NotFoundPage
