f3a34b05300515fe43af77e2c864fed0192fea1c
manu-4216 <manu4216@gmail.com>
2018-09-05 17:41:31 +0200

    Add AdminsListPage, action and reducer

---
+++ src/client/pages/AdminsListPage.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchAdmins } from '../actions'
+
+class AdminsListPage extends Component {
+  componentDidMount() {
+    this.props.fetchAdmins()
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Protectedd list of admins</h3>
+        <ul>{this.renderAdmins()}</ul>
+      </div>
+    )
+  }
+
+  renderAdmins() {
+    return this.props.admins.map(admin => {
+      return <li key={admin.id}>{admin.name}</li>
+    })
+  }
+}
+
+const mapStateToProps = ({ admins }) => ({ admins })
+
+export default {
+  component: connect(
+    mapStateToProps,
+    { fetchAdmins }
+  )(AdminsListPage),
+  loadData: ({ dispatch }) => dispatch(fetchAdmins())
+}
